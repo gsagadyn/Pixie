3debdf4e28f90a6426e5dc4b0731c73fa4444296
Grzegorz Sagadyn <sagadyn@gmail.com>
2020-04-03 11:06:09 +0200

    Release 1.0.4.

---
+++ Pixie/Pixie/Classes/Tools/String/String+SneakCase.swift
@@ -0,0 +1,21 @@
+//
+//  String+SneakCase.swift
+//  Pixie
+//
+//  Created by Grzegorz Sagadyn on 03/04/2020.
+//
+
+import Foundation
+
+extension String {
+    private static var uppercaseLetters = CharacterSet.uppercaseLetters
+    
+    internal var snakeCase: String {
+        unicodeScalars.reduce("") {
+            let isUppercased = Self.uppercaseLetters.contains($1)
+            let currentItem = isUppercased ? String($1).lowercased() : String($1)
+            let separator = $0.isEmpty || !isUppercased ? "" : "_"
+            return "\($0)\(separator)\(currentItem)"
+        }
+    }
+}
