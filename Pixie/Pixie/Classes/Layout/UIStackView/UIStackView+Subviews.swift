092e09918b709c17b0cfc0abe5a6e7a454b735f9
Grzegorz Sagadyn <sagadyn@gmail.com>
2020-04-07 14:45:31 +0200

    Release 1.1.1.

--- Pixie/Pixie/Classes/Layout/UIStackView/UIStackView+Subviews.swift
+++ Pixie/Pixie/Classes/Layout/UIStackView/UIStackView+Subviews.swift
@@ -9,10 +9,18 @@ import UIKit
 
 extension UIStackView {
     public func addArrangedSubviews(_ subviews: UIView...) {
+        addArrangedSubviews(subviews)
+    }
+    
+    public func addArrangedSubviews(_ subviews: [UIView]) {
         subviews.forEach(addArrangedSubview)
     }
     
     public func reloadArrangedSubviews(_ subviews: UIView...) {
+        reloadArrangedSubviews(subviews)
+    }
+    
+    public func reloadArrangedSubviews(_ subviews: [UIView]) {
         guard subviews != arrangedSubviews else { return }
         
         let copy = arrangedSubviews
