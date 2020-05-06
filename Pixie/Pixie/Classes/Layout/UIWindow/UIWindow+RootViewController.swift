6251cc47a538daa6856ff28367fdc3db9ba3c262
Grzegorz Sagadyn <sagadyn@gmail.com>
2020-05-06 14:40:05 +0200

    Fix setrootviewcontroller animation

--- Pixie/Pixie/Classes/Layout/UIWindow/UIWindow+RootViewController.swift
+++ Pixie/Pixie/Classes/Layout/UIWindow/UIWindow+RootViewController.swift
@@ -7,72 +7,45 @@
 
 import UIKit
 
-public protocol RootViewControllerAnimator {
-    func animate(from: UIViewController?, to: UIViewController?, inside view: UIView, completed: @escaping () -> Void)
-}
-
 extension UIWindow {
     public func setRootViewController(
         _ viewController: UIViewController,
         withAnimationDuration duration: TimeInterval,
         delay: TimeInterval = 0.0,
-        options: UIView.AnimationOptions = [.curveEaseInOut],
+        type: CATransitionType = .fade,
+        subtype: CATransitionSubtype? = nil,
+        timingFunction: CAMediaTimingFunctionName = .easeInEaseOut,
         completion: ((Bool) -> Void)? = nil) {
         
-        let animator = DefaultAnimator(animationDuration: duration, delay: delay, options: options)
-        setRootViewController(viewController, animator: animator)
-    }
-
-    public func setRootViewController(_ viewController: UIViewController, animator: RootViewControllerAnimator?) {
-        let fromVc = rootViewController
-        let toVc = viewController
-
-        guard fromVc !== toVc else { return }
-
-        rootViewController = toVc
-
-        guard UIApplication.shared.applicationState == .active else { return }
-
-        let animationView = UIView(frame: toVc.view.bounds)
-        animationView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
-        animationView.backgroundColor = .clear
-        toVc.view.addSubview(animationView)
-
-        let onCompleted: () -> Void = {
-            animationView.removeFromSuperview()
-        }
-
-        animator == nil ? onCompleted() : Void()
-        animator?.animate(from: fromVc, to: toVc, inside: animationView, completed: onCompleted)
-    }
-}
-
-// -----------------------------------------------------------------------------
-// MARK: - Default Animator
-// -----------------------------------------------------------------------------
-
-fileprivate struct DefaultAnimator: RootViewControllerAnimator {
-    // MARK: - Fileprivate Properties
-
-    fileprivate let animationDuration: TimeInterval
-    fileprivate let delay: TimeInterval
-    fileprivate let options: UIView.AnimationOptions
-
-    // MARK: - Embedded View Controller Animator
-
-    fileprivate func animate(from: UIViewController?, to: UIViewController?, inside view: UIView, completed: @escaping () -> Void) {
-        guard let fromView = from?.view, !animationDuration.isZero else {
-            completed()
+        guard rootViewController !== viewController else {
+            completion?(false)
             return
         }
 
-        let snapshot = fromView.snapshotView(afterScreenUpdates: true) ?? UIView(frame: fromView.bounds)
-        view.addSubview(snapshot)
+        guard duration.isNormal && UIApplication.shared.applicationState == .active else {
+            rootViewController = viewController
+            completion?(true)
+            return
+        }
 
-        UIView.animate(withDuration: animationDuration, delay: delay, options: options, animations: {
-            snapshot.alpha = 0.0
-        }, completion: { _ in
-            completed()
-        })
+        CATransaction.begin()
+        let transition = CATransition()
+        transition.duration = duration
+        transition.type = type
+        transition.subtype = subtype
+        transition.isRemovedOnCompletion = false
+        transition.timingFunction = .init(name: timingFunction)
+        transition.fillMode = .both
+
+        if delay > 0.0 {
+            let currentTime = CACurrentMediaTime()
+            let layerTime = layer.convertTime(currentTime, from: nil)
+            transition.beginTime = layerTime + delay
+        }
+
+        CATransaction.setCompletionBlock { completion?(true) }
+        layer.add(transition, forKey: nil)
+        rootViewController = viewController
+        CATransaction.commit()
     }
 }
