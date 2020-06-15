dec4b837e38edbec55466297a35bc1219f03ea65
Grzegorz Sagadyn <sagadyn@gmail.com>
2020-06-15 17:39:37 +0200

    Add UIEdgeInsets extensions

--- Pixie/Pixie/Classes/Layout/CGPoint/CGPoint+Operators.swift
+++ Pixie/Pixie/Classes/Layout/CGPoint/CGPoint+Operators.swift
@@ -9,10 +9,10 @@ import UIKit
 
 extension CGPoint {
     public static func + (lhs: CGPoint, rhs: CGPoint) -> CGPoint {
-        return CGPoint(x: lhs.x + rhs.x, y: lhs.y + rhs.y)
+        CGPoint(x: lhs.x + rhs.x, y: lhs.y + rhs.y)
     }
-    
+
     public static func - (lhs: CGPoint, rhs: CGPoint) -> CGPoint {
-        return CGPoint(x: lhs.x - rhs.x, y: lhs.y - rhs.y)
+        CGPoint(x: lhs.x - rhs.x, y: lhs.y - rhs.y)
     }
 }
